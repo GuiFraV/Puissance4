ce4a4088976e8fdecbfbc2313db9fd7e8f6ad081
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-10 20:44:48 +0200

    add nameselector

--- src/front/App.tsx
+++ src/front/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <NameSelector />
+        <NameSelector onSelect={() => null}/>
       </div>
     </>
   )
--- src/front/screen/NameSelector.tsx
+++ src/front/screen/NameSelector.tsx
@@ -1,8 +1,38 @@
-export function NameSelector(){
-    return <form action=''>
-        <label htmlFor="name">Votre pseudo</label>
-        <input type='text' id='name' name='name'></input>
+import { FormEvent, useState } from "react"
 
-        <button>Choisir</button>
-    </form>
+type NameSelectorProps = {
+    onSelect: (name:string) => void
+}
+
+
+export function NameSelector({onSelect}: NameSelectorProps){
+
+    const [error, setError] = useState('')
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        const name = new FormData(e.currentTarget as HTMLFormElement).get('name')
+        if(!name || name.toString() === ''){
+            setError('Vous devez choisir un pseudo')
+            return;
+        }
+        onSelect(name.toString())
+    
+    }
+
+
+    return <>
+    {error && 
+        <div className='alert'>
+            {error}
+            <div className="alert__close">&times;</div>
+        </div>
+    }
+        <form action='' onSubmit={handleSubmit}>
+            <label htmlFor="name">Votre pseudo</label>
+            <input type='text' id='name' name='name' required></input>
+
+            <button>Choisir</button>
+        </form>
+    </>
 }
