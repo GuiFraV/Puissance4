fd1ad852c08b5b38dc455594445dbbbcd95e4bf9
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-11 14:03:54 +0200

    add grille and some css with grid

--- src/front/screen/ColorSelector.tsx
+++ src/front/screen/ColorSelector.tsx
@@ -1,14 +1,10 @@
+import { discColorClass } from "../../func/color"
 import { Player, PlayerColor } from "../../types"
 
 type ColorSelectorProps = {
     onSelect: (color: PlayerColor) => void
     players: Player[],
     colors: PlayerColor[],
-}
-
-
-function disColor(color: PlayerColor){
-    return `disc disc-${color === PlayerColor.YELLOW ? 'yellow' : 'red'}`
 }
 
 export function ColorSelector({ onSelect, players, colors } : ColorSelectorProps){
@@ -18,13 +14,13 @@ export function ColorSelector({ onSelect, players, colors } : ColorSelectorProps
      <div className="players">
         {players.map(player => <div key={player.id} className="player">
             {player.name}
-            {player.color && <div className={disColor(player.color)}></div>}
+            {player.color && <div className={discColorClass (player.color)}></div>}
 
         </div>)}
     </div>
     <h3>Sélectionnez une couleur</h3>
     <div className="selector">
-        {colors.map(color => <button className={disColor(color)} key={color} onClick={() => onSelect(color)}></button>)}
+        {colors.map(color => <button className={discColorClass (color)} key={color} onClick={() => onSelect(color)}></button>)}
     </div>
     </>
 }
--- src/front/screen/Grid.tsx
+++ src/front/screen/Grid.tsx
@@ -1,13 +1,20 @@
 import { CSSProperties } from "react"
 import { CellState, GridState, PlayerColor } from "../../types"
+import { discColorClass } from "../../func/color"
 
 type GridProps = {
     grid: GridState
 }
 
 export function Grid({grid}: GridProps){
+    const cols = grid[0].length
     return <div className="grid" style={{'--rows': grid.length, '--cols':grid[0].length} as CSSProperties}>
         {grid.map((row, y) => row.map((c, x) => <Cell x={x} y={y} color={c} key={`${x} - ${y}`} />))}
+
+        <div className="columns">
+            {new Array(cols).fill(1).map((_, k) => <Column key={k}/>)}
+        </div>
+    
     </div>
 }
 
@@ -19,7 +26,21 @@ type CellProps = {
 
 
 function Cell({x, y, color} : CellProps){
-    return <div>
-        {x} - {y}
+    return <div 
+    style={{'--row': y} as CSSProperties}
+        className={discColorClass(color)}
+    />
+      
+}
+
+type ColumnProps = {
+
+}
+
+function Column({}: ColumnProps){
+
+    return <div className="column">
+        <div className="disc disc-yellow"></div>
     </div>
+
 }
