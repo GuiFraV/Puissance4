fa725e62273bd87ec2c3a3c42ba172ed4642fddb
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-12 17:38:31 +0200

    add Lobby and connect Game Machine with front

---
+++ src/front/hooks/useGame.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren, createContext, useContext } from "react"
+import { GameContext, GameEvents, GameStates } from "../../types"
+import { GameMachine } from "../../machine/GameMachine"
+import { useMachine } from '@xstate/react'
+
+type GameContextType = {
+    state: GameStates,
+    context: GameContext,
+    send: (event: GameEvents) => void,
+    can: (event: GameEvents) => boolean
+}
+
+const Context = createContext<GameContextType>({} as any)
+
+
+export function useGame(): GameContextType {
+    return useContext(Context)
+
+}
+
+export function GameContextProvider({ children }: PropsWithChildren) {
+    const [state, send] = useMachine(GameMachine)
+    return <Context.Provider value={({
+        state: state.value as GameStates,
+        context: state.context,
+        send: send,
+        can: () => false
+    })}>
+        { children }
+        </Context.Provider>
+}
