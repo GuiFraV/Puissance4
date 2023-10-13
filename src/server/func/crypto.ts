81c4506096efc592a5a8e0e483f9caa7e81538fa
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-13 14:32:24 +0200

    setup privatekey

--- src/server/func/crypto.ts
+++ src/server/func/crypto.ts
@@ -1,12 +1,14 @@
 import * as Cryoto from 'crypto'
+import { readFileSync } from 'fs'
 
 const algo = "SHA256"
+const privateKey = readFileSync('privatekey.pem')
 
 export function sign(
     str: string
 ): string {
 
-    Crypto.sign(algo, str,)
+    Crypto.sign(algo, Buffer.from(str), privateKey)
 }
 
 export function verify(
