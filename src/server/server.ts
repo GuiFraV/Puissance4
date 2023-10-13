70a48d0a40f3958335e752e4bb45797e11022e74
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-13 13:35:27 +0200

    setup fastify

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,12 @@
-console.log("hello World")
+import Fastify from 'fastify'
+
+const fastify = Fastify({ logger: true })
+
+fastify.listen({ port: 8000 }).catch((err) => {
+    fastify.log.error(err)
+    process.exit(1)
+}).then(() => {
+    fastify.log.info('Le serveur écoute sur le port 8000')
+})
+
+console.log("hello")
